eebda6a58071e66c23cb2b5e6da0d113f372ad9f
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-04-04 06:09:37 +0000

    tin

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -20,7 +20,7 @@ class FizzBuzzTest < Minitest::Test
     assert_equal '4', fizz_buzz(4)
     assert_equal 'Buzz', fizz_buzz(5)
     assert_equal 'Fizz', fizz_buzz(6)
-    assert_equal 'Fizz Buzz', fizz_buzz(15)
+    assert_equal 'Fizz Buzz', fizz_buzz(16)
   end
 end
 
